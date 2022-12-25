feb99e91b90f398491610bc84febeab21628fd9a
obadaq <obadaq@ppu.edu>
2022-12-25 18:45:46 +0200

    Almost finished

--- main.py
+++ main.py
@@ -39,19 +39,48 @@ def plot_pie(location):
 
 def plot_bars():
 
-    a1 = crime_spain_df.groupby(['Location']).sum()['Total cases']
-    plt.xlabel('Locations')
-    plt.ylabel('Total Cases')
-    plt.title('Crimes in Spain"By City"')
-    plt.xticks(np.arange(len(locations)), rotation=45)
-    plt.bar(locations, a1)
+    barWidth = 0.25
+    fig,axes = plt.subplots(2,1)
+ 
+    crime1_tc_bylocation = crime_spain_df[crime_spain_df['Crime'] == crimes[0]].groupby('Location').sum()['Total cases']
+    crime2_tc_bylocation = crime_spain_df[crime_spain_df['Crime'] == crimes[1]].groupby('Location').sum()['Total cases']
+    crime3_tc_bylocation = crime_spain_df[crime_spain_df['Crime'] == crimes[2]].groupby('Location').sum()['Total cases']
+
+
+    br1 = np.arange(len(crime1_tc_bylocation))
+    br2 = [x + barWidth for x in br1]
+    br3 = [x + barWidth for x in br2]
+
+    axes[0].bar(br1, crime1_tc_bylocation, color ='r', width = barWidth,
+        edgecolor ='grey', label =crimes[0])
+    axes[0].bar(br1, crime2_tc_bylocation,bottom=crime1_tc_bylocation, color ='g', width = barWidth,
+        edgecolor ='grey', label =crimes[1])
+    axes[0].bar(br1, crime3_tc_bylocation,bottom=crime2_tc_bylocation+crime1_tc_bylocation, color ='b', width = barWidth,
+        edgecolor ='grey', label =crimes[2])
+
+    axes[0].set_xlabel('Locations', fontweight ='bold', fontsize = 15)
+    axes[0].set_ylabel('Total Cases', fontweight ='bold', fontsize = 15)
+    axes[0].set_xticks([r + barWidth for r in range(len(crime1_tc_bylocation))], locations,rotation= 45)
+    axes[0].set_title('Crime Comparison in Spain',fontweight='bold')
+    axes[0].legend()
+
+    axes[1].bar(br1, crime1_tc_bylocation, color ='r', width = barWidth,
+        edgecolor ='grey', label =crimes[0])
+    axes[1].bar(br2, crime2_tc_bylocation, color ='g', width = barWidth,
+        edgecolor ='grey', label =crimes[1])
+    axes[1].bar(br3, crime3_tc_bylocation, color ='b', width = barWidth,
+        edgecolor ='grey', label =crimes[2])
+
+    axes[1].set_xlabel('Locations', fontweight ='bold', fontsize = 15)
+    axes[1].set_ylabel('Total Cases', fontweight ='bold', fontsize = 15)
+    axes[1].set_xticks([r + barWidth for r in range(len(crime1_tc_bylocation))], locations,rotation= 45)
     plt.show()
 
 
 print('''
 1. show trends for crimes in spain (2019-2021)
 2. show pie chart for crimes in a city of your choice
-3. show bar chart for total crimes in spain in general
+3. show bar chart for total cases in spain with three RANDOM crimes 
 ''')
 user_ch = int(input('Enter your choice >>> '))
 
--- test3.py
+++ test3.py
@@ -13,35 +13,43 @@ locations = list(set(crime_spain_df['Location']))
 #tc= crime_spain_df.groupby('Location').sum()['Total cases']
 
 
-# set width of bar
-barWidth = 0.25
-fig = plt.subplots(figsize =(12, 8))
+def plot_bars():
+
+    barWidth = 0.25
+    fig,axes = plt.subplots(2,1)
  
-# set height of bar
+    crime1_tc_bylocation = crime_spain_df[crime_spain_df['Crime'] == crimes[0]].groupby('Location').sum()['Total cases']
+    crime2_tc_bylocation = crime_spain_df[crime_spain_df['Crime'] == crimes[1]].groupby('Location').sum()['Total cases']
+    crime3_tc_bylocation = crime_spain_df[crime_spain_df['Crime'] == crimes[2]].groupby('Location').sum()['Total cases']
 
-crime1_tc_bylocation = list(crime_spain_df[crime_spain_df['Crime'] == crimes[0]].groupby('Location').sum())
-crime2_tc_bylocation = list(crime_spain_df[crime_spain_df['Crime'] == crimes[1]].groupby('Location').sum())
-crime3_tc_bylocation = list(crime_spain_df[crime_spain_df['Crime'] == crimes[2]].groupby('Location').sum())
 
-# Set position of bar on X axis
+    br1 = np.arange(len(crime1_tc_bylocation))
+    br2 = [x + barWidth for x in br1]
+    br3 = [x + barWidth for x in br2]
 
-br1 = np.arange(len(crime1_tc_bylocation))
-br2 = [x + barWidth for x in br1]
-br3 = [x + barWidth for x in br2]
- 
-# Make the plot
-plt.bar(br1, crime1_tc_bylocation, color ='r', width = barWidth,
+    axes[0].bar(br1, crime1_tc_bylocation, color ='r', width = barWidth,
         edgecolor ='grey', label =crimes[0])
-plt.bar(br2, crime2_tc_bylocation, color ='g', width = barWidth,
+    axes[0].bar(br1, crime2_tc_bylocation,bottom=crime1_tc_bylocation, color ='g', width = barWidth,
         edgecolor ='grey', label =crimes[1])
-plt.bar(br3, crime3_tc_bylocation, color ='b', width = barWidth,
+    axes[0].bar(br1, crime3_tc_bylocation,bottom=crime2_tc_bylocation+crime1_tc_bylocation, color ='b', width = barWidth,
         edgecolor ='grey', label =crimes[2])
 
+    axes[0].set_xlabel('Locations', fontweight ='bold', fontsize = 15)
+    axes[0].set_ylabel('Total Cases', fontweight ='bold', fontsize = 15)
+    axes[0].set_xticks([r + barWidth for r in range(len(crime1_tc_bylocation))], locations,rotation= 45)
+    axes[0].set_title('Crime Comparison in Spain',fontweight='bold')
+    axes[0].legend()
 
-# Adding Xticks
-plt.xlabel('Locations', fontweight ='bold', fontsize = 15)
-plt.ylabel('Total Cases', fontweight ='bold', fontsize = 15)
-plt.xticks([r + barWidth for r in range(len(crime1_tc_bylocation))], locations)
- 
-plt.legend()
-plt.show()
+    axes[1].bar(br1, crime1_tc_bylocation, color ='r', width = barWidth,
+        edgecolor ='grey', label =crimes[0])
+    axes[1].bar(br2, crime2_tc_bylocation, color ='g', width = barWidth,
+        edgecolor ='grey', label =crimes[1])
+    axes[1].bar(br3, crime3_tc_bylocation, color ='b', width = barWidth,
+        edgecolor ='grey', label =crimes[2])
+
+    axes[1].set_xlabel('Locations', fontweight ='bold', fontsize = 15)
+    axes[1].set_ylabel('Total Cases', fontweight ='bold', fontsize = 15)
+    axes[1].set_xticks([r + barWidth for r in range(len(crime1_tc_bylocation))], locations,rotation= 45)
+    plt.show()
+
+plot_bars()
